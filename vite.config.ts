ae95041355d5428850ca4ec1c3eaf24e5d64e888
shashankpawan <shashank.pawan@gmail.com>
2025-07-26 19:29:53 +0530

    CHanges for GH-pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,10 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./attached_assets")
     }
   },
-  root: path.resolve(__dirname, "client"),
+  root: path.resolve(__dirname, "client"), 
   build: {
     outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
-    assetsDir: "assets" // Ensures consistent asset paths
+   // emptyOutDir: true,
+   // assetsDir: "assets" // Ensures consistent asset paths
   }
 });
