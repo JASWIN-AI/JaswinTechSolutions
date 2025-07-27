e447c36f952841035f2f4fef1dd10b2ef20a8b9a
JeetGitPilot <mydearudear@gmail.com>
2025-07-27 16:17:55 +0530

    imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import About from "@/pages/about";
 import Services from "@/pages/services";
 import Projects from "@/pages/projects";
 import Contact from "@/pages/contact";
-import NotFound from "@/pages/not-found";
+import "@/styles/globals.css";
 
 function Router() {
   return (
